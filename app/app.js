f5f28f6ba1c8d5dd4284b7f17db57d8944e04688
unknown <tempkr007@gmail.com>
2024-04-20 11:42:39 +0900

    update

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 //라우팅
-const home = require("./routes/home") //home에 있는 js 를 가져옴
+const home = require("./src/routes/home") //home에 있는 js 를 가져옴
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
 app.use("/", home); //미들 웨어를 등록해주는 메서드
