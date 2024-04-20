f5dc9d5daef02eecaa30ab7cf2182d88ab992ad3
unknown <tempkr007@gmail.com>
2024-04-20 19:10:35 +0900

    로그인을 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,6 +10,14 @@ function login() {
         id : id.value,
         psword : psword.value,
     };
-    console.log(req)
+    console.log(req,JSON.stringify(req));
+
+    fetch("/login",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 
 }
