3a7e4dee0876af3aaade2425912a47683c99ccc5
unknown <tempkr007@gmail.com>
2024-04-20 16:24:19 +0900

    dome control

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,15 @@
-console.log('hello');
-console.log('test');
+const id = document.querySelector("#id"),
+      psword = document.querySelector("#psword"),
+      loginBtn = document.querySelector("button");
+
+
+    loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req)
+
+}
