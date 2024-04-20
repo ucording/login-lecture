85fb0503e319cef48cc37fbd8f92554b2996af9a
unknown <tempkr007@gmail.com>
2024-04-20 22:12:45 +0900

    erroer 처리

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,6 +18,18 @@ function login() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        if (res.success) {
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        // console.error(new Error("로그인 중 에러 발생"))
+        console.error("로그인 중 에러 발생")
+    }); 
 
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,43 @@
-const hello = (req,res) => {
-    res.render("home/index")
+"use strict";
+
+const output = {
+    home: (req,res) => {
+        res.render("home/index")
+    },
+    
+    login: (req,res) => {
+        res.render("home/login")
+    },
+};
+const users = {
+    id: ["worimIT", "나개발", "김팀장"],
+    psword: ["1234", "1234", "123456"],
 };
 
-const login = (req,res) => {
-    res.render("home/login")
+const process = {
+    login: (req,res) => {
+        const id = req.body.id,
+          psword = req.body.psword;
+        
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json ({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다",
+        });
+
+    },
 };
+
 
 // 밖에서 사용하려면 export 해줘야 함
 module.exports = {
-    hello,   //key 값만 있는경우 key : key, 키 값이 생김
-    login,
+    output,
+    process,
 };
