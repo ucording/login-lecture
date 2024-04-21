19c177bca738b372af338423d3ef6e3c6d08a56c
unknown <tempkr007@gmail.com>
2024-04-21 20:40:11 +0900

    register add

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -15,13 +15,17 @@ const output = {
     }
 };
 
-
 const process = {
     login: (req, res) => {
         const user = new User(req.body);
         const response = user.login();
         return res.json(response);
     },
+    register: (req, res) => {
+        const user = new User(req.body);
+        const response = user.register();
+        return res.json(response);        
+    }
 };
 
 
