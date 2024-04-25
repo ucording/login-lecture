f85ec45f1053ccc16a0fe8df36259b8ed1a377fe
unknown <tempkr007@gmail.com>
2024-04-25 23:09:40 +0900

    add log file by winston

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const app = require("../app")
+const logger = require("../src/config/logger");
 const PORT = process.env.PORT || 3000;  // || or 연산자 앞에값이 있으면 true 반환해서 앞값을 가져오고 값이 없으면 false 를 반환해서 뒤에오는 3000 값을 사용함
 
 app.listen(PORT, ()=>{
-    console.log('서버 가동 : 3000')
+    logger.info(`${PORT} 포트에서 서버가 가동되었습니다`)
 })
